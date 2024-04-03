d40f00b36cb962224bef7768c9816874290c5876
Joshua Hodge <josh@theaudioprogrammer.com>
2024-04-03 06:28:06 -0400

    Adding basic implementation

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,79 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    // Create some options to configure the webview
+    choc::ui::WebView::Options options;
+    options.enableDebugMode = true;
+    options.acceptsFirstMouseClick = true;
+
+    // Initialize the webview...
+    webView = std::make_unique<choc::ui::WebView>(options);
+
+    // Platform specific container initialization, then set our webview...
+#if JUCE_MAC
+    webViewContainer = std::make_unique<juce::NSViewComponent>();
+    webViewContainer->setView (webView->getViewHandle());
+#elif JUCE_WINDOWS
+    webViewContainer = std::make_unique<juce::HWNDComponent>();
+    webViewContainer->setHWND (webView->getViewHandle());
+#endif
+
+    addAndMakeVisible (*webViewContainer);
+
+    /* Lambda to parse through the JS JavaScript and pass it to the processor.
+    This is a "proof of concept" where the only param is our gain.  A true implementation would identify what param
+    was changed, then update accordingly.  I suppose you could alternatively have a separate callback for each param. */
+    auto onGainSliderChanged = [this](const choc::value::ValueView& args) ->choc::value::Value
+    {
+        auto json = juce::JSON::parse (choc::json::toString(args));
+
+        if (json.isArray())
+        {
+            float gainVal = json[0].toString().getFloatValue();
+            processorRef.gain = gainVal;
+        }
+        else
+        {
+            jassertfalse;
+            return choc::value::Value(-1);
+        }
+
+        return choc::value::Value(0);
+    };
+
+    /* Here, we are looking for a callback in our JS called gainValueCallback(). This is invoked on the web view's
+     * slider change.  Then we call our C++ lambda onGainSliderChanged() to pass on the gain value */
+    webView->bind ("gainValueCallback", onGainSliderChanged);
+
+    // Find our html file from our binary data
+    const auto htmlData = juce::String::createStringFromData (data::gain_view_html, data::gain_view_htmlSize);
+
+    // If valid, show the HTML
+    if (htmlData.isNotEmpty())
+        webView->setHTML (htmlData.toStdString());
+    else
+        jassertfalse;
+
+    setSize (400, 300);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void AudioPluginAudioProcessorEditor::resized()
+{
+    webViewContainer->setBounds (getLocalBounds());
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "PluginProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
+#include "gui/choc_WebView.h"
+#include "Data.h"
+
+//==============================================================================
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+    ~AudioPluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    AudioPluginAudioProcessor& processorRef;
+
+    // Platform specific containers
+#if JUCE_MAC
+    std::unique_ptr<juce::NSViewComponent> webViewContainer;
+#elif JUCE_WINDOWS
+    std::unique_ptr<juce::HWNDComponent> webViewContainer;
+#endif
+
+    // Create choc webview
+    std::unique_ptr<choc::ui::WebView> webView;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+};
